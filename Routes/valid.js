3564f2578cf27c17ace26db947fbff40c74a49fe
anjuman raj <ayusmann23@gmail.com>
2022-11-06 11:57:34 +0530

    otp setup

--- Routes/valid.js
+++ Routes/valid.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getOTP } = require('../Controller/validController');
+const { getOTP, verifyOTP } = require('../Controller/validController');
 
 
 router.post('/sendotp', getOTP);
+router.post('/verifyotp', verifyOTP);
+// router.post('/email',sendMail)
 
 module.exports = router;
